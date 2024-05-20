d078621b65bb7a364853dc721946fdf02949f14c
Shreyas Kapur <shreyaskapur8@gmail.com>
2024-05-19 22:55:13 -0700

    tldr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,10 @@
 <script>
 	let heroVideos = [
-		{ video: '/videos/sketch1.mp4', title: 'Sketch 1', thumbnail: '/videos/sketch1.png' },
 		{ video: '/videos/sketch4.mp4', title: 'Sketch 4', thumbnail: '/videos/sketch4.png' },
-		{ video: '/videos/sketch2.mp4', title: 'Sketch 2', thumbnail: '/videos/sketch2.png' },
+		{ video: '/videos/sketch1.mp4', title: 'Sketch 1', thumbnail: '/videos/sketch1.png' },
+		{ video: '/videos/sketch5.mp4', title: 'Sketch 5', thumbnail: '/videos/sketch5.png' },
 		{ video: '/videos/sketch3.mp4', title: 'Sketch 3', thumbnail: '/videos/sketch3.png' },
-		{ video: '/videos/sketch5.mp4', title: 'Sketch 5', thumbnail: '/videos/sketch5.png' }
+		{ video: '/videos/sketch2.mp4', title: 'Sketch 2', thumbnail: '/videos/sketch2.png' }
 	];
 
 	let currentVideo = heroVideos[0];
@@ -25,6 +25,10 @@
 <div class="flex w-full justify-center pt-8 font-serif">
 	<div class="mx-8 max-w-4xl w-full">
 		<div class="w-full flex justify-center items-center flex-col">
+			<div class="pb-4">
+				<strong>TLDR;</strong> We teach neural models to *edit* programs. <br /> After each edit, the
+				model can look at the output.
+			</div>
 			<div class="border-2 border-gray-300 border-solid inline-block p-8 rounded-md">
 				<video src={currentVideo.video} autoplay loop muted class="max-w-xl w-full"></video>
 			</div>
